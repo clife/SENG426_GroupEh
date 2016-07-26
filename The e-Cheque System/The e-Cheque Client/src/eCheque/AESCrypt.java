8acc2a12187a14384059282e65dfb9420faf657b
Tristan Lucas <tlucas.u@gmail.com>
2016-07-25 23:18:16 -0700

    Fixed AESCrypt

--- The e-Cheque System/The e-Cheque Client/src/eCheque/AESCrypt.java
+++ The e-Cheque System/The e-Cheque Client/src/eCheque/AESCrypt.java
@@ -17,7 +17,6 @@ import java.io.*;
 
 /** Creates a new instance of SecretKeyGenerateRandomAESKey */
 public class AESCrypt {
-<<<<<<< HEAD
        
     public SecretKey generateRandomAESKey() throws Exception {
 
@@ -97,89 +96,4 @@ public class AESCrypt {
        aesKey = new SecretKeySpec(keyData, "AES");
        return aesKey;
     }
-=======
-	
-	
-	public SecretKey GenerateRandomAESKey() throws Exception {
-
-		KeyGenerator KeyGen = KeyGenerator.getInstance("AES");
-		SecureRandom random = new SecureRandom();
-		KeyGen.init(random);
-		SecretKey key = KeyGen.generateKey();
-		
-		return key;
-	}
-	
-	
-	public Cipher initializeCipher(Key key, int mode) throws Exception{
-
-		int cipherMode;
-		Cipher cipherObj; 
-
-		if (mode == 0) {
-			cipherMode = Cipher.ENCRYPT_MODE;
-		}
-		else if (mode == 1) {
-			cipherMode = Cipher.DECRYPT_MODE;
-		}
-		else if (mode == 2) {
-			cipherMode = Cipher.WRAP_MODE;
-		}
-		else {
-			cipherMode = Cipher.UNWRAP_MODE;
-		}   
-
-		if((mode == 0) || (mode == 1)) {
-			cipherObj = Cipher.getInstance("AES");
-		}
-		else {
-			cipherObj =Cipher.getInstance("RSA");
-		}
-		
-		cipherObj.init(cipherMode,key);
-		
-		return cipherObj;
-	}
-	
-	
-	public void crypt(InputStream inStream, OutputStream outStream, Cipher cipherObj) throws Exception {
-		
-		int blockSize = cipherObj.getBlockSize();
-		int outputSize = cipherObj.getOutputSize(blockSize);
-		byte[] inBytes = new byte[blockSize];
-		byte[] outBytes = new byte[outputSize];
-		int inLength = 0;
-		boolean more = true;
-
-		while (more) {
-			inLength = inStream.read(inBytes);
-			
-			if(inLength == blockSize) {
-				int outLength = cipherObj.update(inBytes, 0, blockSize, outBytes);
-				outStream.write(outBytes, 0, outLength);
-			}
-			else {
-				more = false ;
-			}
-		}
-		
-		if (inLength > 0) {
-			outBytes = cipherObj.doFinal(inBytes, 0, inLength);
-		}
-		else {
-			outBytes = cipherObj.doFinal();
-		}
-		
-		outStream.write(outBytes);
-	}
-	
-	
-	public SecretKeySpec inilizeAESKeyByPassword(String password){
-		byte[] KeyData = password.getBytes();
-		
-		SecretKeySpec aesKey = new SecretKeySpec(KeyData,"AES");
-		
-		return aesKey;
-	}
->>>>>>> origin/master
 }
